820549a047e71324cf5e3b5a3e696e57b8f0121b
GaneshIT <ganesh.ceoit@gmail.com>
2022-01-07 15:47:15 +0530

    updated sales file

---
+++ updated sales query 2.sql
@@ -0,0 +1,37 @@
+
+create table orders(ord_no numeric(5) primary key, purch_amt decimal(8,2), ord_date date, customer_id numeric(5) references customer(customer_id), salesman_id numeric(5) references salesman(salesman_id))
+
+drop table orders
+
+insert into orders values(70001, 150.5, '2012-10-05', 3005, 5002)
+insert into orders values(70009, 270.65, '2012-09-10', 3001, 5005)
+insert into orders values(70002, 65.26, '2012-10-05', 3002, 5001)
+insert into orders values(70004, 110.5, '2012-08-17', 3009, 5003)
+insert into orders values(70007, 948.5, '2012-09-10', 3005, 5002)
+insert into orders values(70005, 2400.6, '2012-07-27', 3007, 5001)
+insert into orders values(70008, 5760, '2012-09-10', 3002, 5001)
+insert into orders values(70010, 1983.43, '2012-10-10', 3004, 5006)
+insert into orders values(70003, 2480.4, '2012-10-10', 3009, 5003)
+insert into orders values(70012, 250.45, '2012-06-27', 3008, 5002)
+insert into orders values(70011, 75.29, '2012-08-17', 3003, 5007)
+insert into orders values(70013, 3045.6, '2012-04-25', 3002, 5001)
+
+select * from orders
+
+select avg(purch_amt) from orders
+select max(purch_amt) from orders
+select customer_id, max(purch_amt) from orders group by customer_id
+
+select ord_no, ord_date, purch_amt from orders where salesman_id=5001
+
+select ord_date, salesman_id, ord_no, purch_amt from orders
+
+select name, city from salesman where city= 'Paris'
+
+select name, commission from salesman 
+
+select count(ord_no) from orders where ord_date='2012-08-17'
+
+select salesman_id, max(purch_amt) from orders group by salesman_id having salesman_id between 5003 and 5008
+
+select ord_date, salesman_id, count(*) from orders group by ord_date, salesman_id
---
+++ updated sales query1.sql
@@ -0,0 +1,21 @@
+create database sales
+create table salesman(salesman_id numeric(5) primary key , name varchar(30), city varchar(50), commission decimal(5,2))
+
+drop table salesman
+
+insert into salesman values(5001, 'James Hoog', 'New York', 0.15)
+insert into salesman values(5002, 'Nail Knite', 'Paris', 0.13)
+insert into salesman values(5005, 'Pit Alex', 'London', 0.11)
+insert into salesman values(5006, 'Mc Lyon', 'Paris', 0.14)
+insert into salesman values(5007, 'Paul Adam', 'Rome', 0.13)
+insert into salesman values(5003, 'Lauson Hen', 'San Jose', 0.12)
+
+select * from salesman
+
+select name, commission from salesman 
+select name, city from salesman where city= 'Paris'
+select count(distinct salesman_id) from salesman
+
+select count(*) from salesman where city is not null
+
+  
